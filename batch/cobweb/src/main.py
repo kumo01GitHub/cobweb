7e0a3f4cc4f86a106b341ab216acb95bd209523b
kumo01GitHub <kumo01@outlook.com>
2025-03-24 00:13:45 +0900

    feat: batch .env

--- batch/cobweb/src/main.py
+++ batch/cobweb/src/main.py
@@ -1,10 +1,16 @@
 import sys
+from os.path import join, dirname
+from dotenv import load_dotenv
 
 from pyspark.sql import SparkSession
 
 from sample import sample
 
 def main():
+    load_dotenv(verbose=True)
+    dotenv_path = join(dirname(__file__), '../../.env')
+    load_dotenv(dotenv_path)
+
     spark = SparkSession.builder.appName(sys.argv[1]).getOrCreate()
     print("run", spark.conf.get("spark.app.name"))
 
--- batch/cobweb/src/sample/sample.py
+++ batch/cobweb/src/sample/sample.py
@@ -1,25 +1,15 @@
 import os
-from string import Template
 
 def exec(spark):
-    url = Template("jdbc:postgresql://${host}:${port}/${database}")
-
-    df = (
+    jdbc = (
         spark.read.format("jdbc")
-        .option(
-            "url",
-            url.substitute(
-                host=os.environ["DATABASE_HOST"],
-                port=os.environ["DATABASE_PORT"],
-                database=os.environ["DATABASE_NAME"],
-            ),
-        )
+        .option("url", os.environ["SPARK_DATABASE_URL"])
         .option("dbtable", "django.auth_user")
-        .option("user", os.environ["DATABASE_USER"])
-        .option("password", os.environ["DATABASE_PASSWORD"])
+        .option("user", os.environ["SPARK_DATABASE_USER"])
+        .option("password", os.environ["SPARK_DATABASE_PASSWORD"])
         .option("driver", "org.postgresql.Driver")
         .load()
     )
 
-    df.printSchema()
-    df.show(20)
+    jdbc.printSchema()
+    jdbc.show(20)
